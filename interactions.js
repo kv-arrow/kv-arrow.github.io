d962e36e406ca264ccdf23f08bdc55e5e007732c
kv-arrow <114992314+kv-arrow@users.noreply.github.com>
2022-10-31 21:33:13 +0200

    Create interactions.js

---
+++ interactions.js
@@ -0,0 +1,13 @@
+<script>
+LottieInteractivity.create({
+    player: '#firstLottie',
+    mode: 'cursor',
+    actions: [
+        {
+            position: { x: [0, 1], y: [-1, 2] },
+            type: 'seek',
+            frames: [0, 96],
+        }
+    ]
+});
+</script>
