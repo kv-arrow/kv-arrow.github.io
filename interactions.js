5ffbf3c6e6d447b4776dfceb78f14c96fad1a0e9
kv-arrow <114992314+kv-arrow@users.noreply.github.com>
2022-11-13 22:05:23 +0200

    13.11.2022

--- interactions.js
+++ interactions.js
@@ -1,10 +1,32 @@
 // Animation configuration
 
 
-
-
   LottieInteractivity.create({
     player: '#esimerkki1',
+    mode: 'scroll',
+    actions: [
+      {
+         visibility:[0, 1.0] ,
+          type: 'seek',
+        frames: [0, 121]
+      }
+    ]
+  });
+  
+    LottieInteractivity.create({
+    player: '#esimerkki2',
+    mode: 'scroll',
+    actions: [
+   
+	   {
+            visibility:[0.5,1.0],
+            type: "play",
+        }
+    ]
+  });
+
+  LottieInteractivity.create({
+    player: '#esimerkki3',
     mode: 'cursor',
     actions: [
       {
@@ -14,5 +36,46 @@
       }
     ]
   });
+  
+   LottieInteractivity.create({
+    player: '#esimerkki4',
+    mode: 'cursor',
+    actions: [
+      {
+          type: "toggle"
+        
+      }
+    ]
+  });
 
+LottieInteractivity.create({
+    player: '#esimerkki5',
+    mode: 'chain',
+    actions: [
+      {
+          state: 'none',
+          transition: 'click',
+          frames: [0]
+      },
+	  {
+          state: 'autoplay',
+		  ForceFlag: false,
+          frames: [0,60],
+		  transition: 'onComplete'
+      },
+	  {
+         state: 'none',
+          frames: [60],
+		  transition: 'click'
+		 
+      },
+	  {
+          state: 'autoplay',
+		  ForceFlag: false,
+          frames: [60,121],
+		  transition: 'onComplete',
+		   reset: true
+      },
+    ]
+  });
   
