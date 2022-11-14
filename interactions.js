d4a01f81ac88ee2e91b025a1217066dadfb10617
kv-arrow <114992314+kv-arrow@users.noreply.github.com>
2022-11-14 15:26:06 +0200

    14.11.

--- interactions.js
+++ interactions.js
@@ -6,9 +6,9 @@
     mode: 'scroll',
     actions: [
       {
-         visibility:[0, 1.0] ,
+         visibility:[0.2, 1.0] ,
           type: 'seek',
-        frames: [0, 121]
+        frames: [0, 48]
       }
     ]
   });
@@ -19,7 +19,7 @@
     actions: [
    
 	   {
-            visibility:[0.2,1.0],
+            visibility:[0.25, 1.0],
             type: "play",
         }
     ]
@@ -32,7 +32,7 @@
       {
          position: { x: [0, 1], y: [-1, 2] },
           type: 'seek',
-        frames: [0, 121]
+        frames: [0, 48]
       }
     ]
   });
@@ -60,22 +60,46 @@ LottieInteractivity.create({
 	  {
           state: 'autoplay',
 		  ForceFlag: false,
-          frames: [0,60],
+          frames: [0,17],
 		  transition: 'onComplete'
       },
 	  {
          state: 'none',
-          frames: [60],
+          frames: [35],
 		  transition: 'click'
 		 
       },
 	  {
           state: 'autoplay',
 		  ForceFlag: false,
-          frames: [60,121],
+          frames: [35,53],
 		  transition: 'onComplete',
-		   reset: true
       },
+	    {
+         state: 'none',
+          frames: [73],
+		  transition: 'click'
+		 
+      },
+	   {
+          state: 'autoplay',
+		  ForceFlag: false,
+          frames: [73,94],
+		  transition: 'onComplete',
+      },
+	  {
+         state: 'none',
+          frames: [112],
+		  transition: 'click'
+		 
+      },
+	  {
+          state: 'autoplay',
+		  ForceFlag: false,
+          frames: [112,162],
+		  transition: 'onComplete',
+		  reset: true
+      }
     ]
   });
   
